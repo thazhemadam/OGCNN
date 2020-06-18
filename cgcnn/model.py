8c41effa5e23f33dafa88964edacab595f63b440
RishikeshMagar <43094762+RishikeshMagar@users.noreply.github.com>
2020-06-18 16:10:55 -0400

    OFM Model
    We OGCNN model

--- cgcnn/model.py
+++ cgcnn/model.py
@@ -57,7 +57,6 @@ class ConvLayer(nn.Module):
         """
         # TODO will there be problems with the index zero padding?
         N, M = nbr_fea_idx.shape
-        # convolution
         atom_nbr_fea = atom_in_fea[nbr_fea_idx, :]
         total_nbr_fea = torch.cat(
             [atom_in_fea.unsqueeze(1).expand(N, M, self.atom_fea_len),
@@ -74,40 +73,20 @@ class ConvLayer(nn.Module):
         return out
 
 
-class CrystalGraphConvNet(nn.Module):
-    """
-    Create a crystal graph convolutional neural network for predicting total
-    material properties.
-    """
-    def __init__(self, orig_atom_fea_len, nbr_fea_len,
-                 atom_fea_len=64, n_conv=3, h_fea_len=128, n_h=1,
+class OrbitalCrystalGraphConvNet(nn.Module):
+    def __init__(self, orig_atom_fea_len, nbr_fea_len, orig_hot_fea_len,
+                 atom_fea_len, hot_fea_len, h_fea_len, n_conv=3, n_h=1,
                  classification=False):
-        """
-        Initialize CrystalGraphConvNet.
-
-        Parameters
-        ----------
-
-        orig_atom_fea_len: int
-          Number of atom features in the input.
-        nbr_fea_len: int
-          Number of bond features.
-        atom_fea_len: int
-          Number of hidden atom features in the convolutional layers
-        n_conv: int
-          Number of convolutional layers
-        h_fea_len: int
-          Number of hidden features after pooling
-        n_h: int
-          Number of hidden layers after pooling
-        """
-        super(CrystalGraphConvNet, self).__init__()
+        super(OrbitalCrystalGraphConvNet, self).__init__()
         self.classification = classification
-        self.embedding = nn.Linear(orig_atom_fea_len, atom_fea_len)
+        self.embedding1 = nn.Linear(orig_atom_fea_len, hot_fea_len)
+        self.relu = nn.Softplus()
+        self.embedding2 = nn.Linear(hot_fea_len, atom_fea_len)
         self.convs = nn.ModuleList([ConvLayer(atom_fea_len=atom_fea_len,
                                     nbr_fea_len=nbr_fea_len)
                                     for _ in range(n_conv)])
         self.conv_to_fc = nn.Linear(atom_fea_len, h_fea_len)
+
         self.conv_to_fc_softplus = nn.Softplus()
         if n_h > 1:
             self.fcs = nn.ModuleList([nn.Linear(h_fea_len, h_fea_len)
@@ -123,38 +102,14 @@ class CrystalGraphConvNet(nn.Module):
             self.dropout = nn.Dropout()
 
     def forward(self, atom_fea, nbr_fea, nbr_fea_idx, crystal_atom_idx):
-        """
-        Forward pass
-
-        N: Total number of atoms in the batch
-        M: Max number of neighbors
-        N0: Total number of crystals in the batch
-
-        Parameters
-        ----------
-
-        atom_fea: Variable(torch.Tensor) shape (N, orig_atom_fea_len)
-          Atom features from atom type
-        nbr_fea: Variable(torch.Tensor) shape (N, M, nbr_fea_len)
-          Bond features of each atom's M neighbors
-        nbr_fea_idx: torch.LongTensor shape (N, M)
-          Indices of M neighbors of each atom
-        crystal_atom_idx: list of torch.LongTensor of length N0
-          Mapping from the crystal idx to atom idx
-
-        Returns
-        -------
-
-        prediction: nn.Variable shape (N, )
-          Atom hidden features after convolution
-
-        """
-        atom_fea = self.embedding(atom_fea)
+        atom_fea = self.embedding1(atom_fea)
+        atom_fea = self.relu(atom_fea)
+        atom_fea = self.embedding2(atom_fea)
         for conv_func in self.convs:
             atom_fea = conv_func(atom_fea, nbr_fea, nbr_fea_idx)
         crys_fea = self.pooling(atom_fea, crystal_atom_idx)
         crys_fea = self.conv_to_fc(self.conv_to_fc_softplus(crys_fea))
-        crys_fea = self.conv_to_fc_softplus(crys_fea)
+        crys_fea = self.conv_to_fc_softplus(crys_fea)   
         if self.classification:
             crys_fea = self.dropout(crys_fea)
         if hasattr(self, 'fcs') and hasattr(self, 'softpluses'):
@@ -166,22 +121,8 @@ class CrystalGraphConvNet(nn.Module):
         return out
 
     def pooling(self, atom_fea, crystal_atom_idx):
-        """
-        Pooling the atom features to crystal features
-
-        N: Total number of atoms in the batch
-        N0: Total number of crystals in the batch
-
-        Parameters
-        ----------
-
-        atom_fea: Variable(torch.Tensor) shape (N, atom_fea_len)
-          Atom feature vectors of the batch
-        crystal_atom_idx: list of torch.LongTensor of length N0
-          Mapping from the crystal idx to atom idx
-        """
-        assert sum([len(idx_map) for idx_map in crystal_atom_idx]) ==\
-            atom_fea.data.shape[0]
+        assert sum([len(idx_map) for idx_map in crystal_atom_idx]) ==  atom_fea.data.shape[0]
         summed_fea = [torch.mean(atom_fea[idx_map], dim=0, keepdim=True)
                       for idx_map in crystal_atom_idx]
         return torch.cat(summed_fea, dim=0)
+
